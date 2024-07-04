0387a2037f259ebed814eee6bfa4934d38dfe909
Evgeny Skopin <skopinevgeniy@gmail.com>
2024-07-04 14:41:01 +0300

    Fixed errors v3 and add check data

--- OrderManager/OrderManager/Program.cs
+++ OrderManager/OrderManager/Program.cs
@@ -4,15 +4,35 @@ string deliveryDateString = nowDate.AddDays( 3 ).ToString( "d" );
 
 Console.Write( "Введите ваше имя: " );
 string name = Console.ReadLine();
+while ( string.IsNullOrWhiteSpace( name ) )
+{
+    Console.Write( "Некорректное имя. Введите еще раз: " );
+    name = Console.ReadLine();
+}
 
 Console.Write( "Введите количество товара: " );
 string count = Console.ReadLine();
+while ( string.IsNullOrWhiteSpace( count ) || !int.TryParse( count, out int productCount ) || productCount <= 0 )
+{
+    Console.Write( "Некорректное количество товара. Введите еще раз: " );
+    count = Console.ReadLine();
+}
 
 Console.Write( "Введите название товара: " );
 string product = Console.ReadLine();
+while ( string.IsNullOrWhiteSpace( product ) )
+{
+    Console.Write( "Некорректное название товара. Введите еще раз: " );
+    product = Console.ReadLine();
+}
 
 Console.Write( "Введите ваш адрес доставки: " );
 string address = Console.ReadLine();
+while ( string.IsNullOrWhiteSpace( address ) )
+{
+    Console.Write( "Некорректный адрес доставки. Введите еще раз: " );
+    address = Console.ReadLine();
+}
 
 string accept = AcceptOrder( name, product, count, address );
 Console.WriteLine( accept );
@@ -21,7 +41,7 @@ string succession = Console.ReadLine();
 
 if ( succession == "y" )
 {
-    string successOrder = SuccessOrder( name, product, count, address, todaysDate, datePlusThreeDays );
+    string successOrder = SuccessOrder( name, product, count, address, nowDateString, deliveryDateString );
     Console.WriteLine( successOrder );
 }
 else
@@ -29,7 +49,7 @@ else
     Console.WriteLine( "Отмена заказа" );
 }
 
-﻿string SuccessOrder( string name, string product, string count, string address, string todaysDate, string datePlusThreeDays )
+string SuccessOrder( string name, string product, string count, string address, string todaysDate, string datePlusThreeDays )
 {
     return $"{name}! Ваш заказ {product} в количестве {count} оформлен! Ожидайте доставку по адресу {address} к {todaysDate} + {datePlusThreeDays}";
 }
