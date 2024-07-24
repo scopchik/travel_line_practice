62899705603bf221103684fa38ede68b2a5eb03e
Evgeny Skopin <skopinevgeniy@gmail.com>
2024-07-24 11:03:06 +0300

    Change condition

--- FindHotels.sql
+++ FindHotels.sql
@@ -22,4 +22,4 @@ WHERE R.room_number = 145;
 SELECT * FROM dbo.Rooms R
 JOIN dbo.Bookings B ON 
 R.room_id = B.room_id
-WHERE ('2024-07-15' < B.check_in_date) OR (B.check_out_date > '2024-07-15') and availability = 'true';
+WHERE (('2024-07-15' < B.check_in_date) OR (B.check_out_date > '2024-07-15')) and availability = 'true';
